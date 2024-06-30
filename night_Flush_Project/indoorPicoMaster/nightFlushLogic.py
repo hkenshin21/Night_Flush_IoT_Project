0dbda2e0b9fdd011f13b262476957419c05ae2c7
Ches <84792339+hkenshin21@users.noreply.github.com>
2024-06-30 08:56:40 +0200

    Update nightFlushLogic.py

--- night_Flush_Project/indoorPicoMaster/nightFlushLogic.py
+++ night_Flush_Project/indoorPicoMaster/nightFlushLogic.py
@@ -1,56 +1,59 @@
 
-#Logic to switch fan on/off. Sensor to know if it is on? (vibrating?)
-# OPTIONAL: def checkBoostButton() #if pressed then pointTask is -2 degrees
-  
+"""Logic for what is going to happen depending on the weather conditions, 
+    forecast and indoor temperature """ 
+#Initialize setPointTemperature to cool day settings:
 setPointTemp = 23
-# Calculate setpoint temperature based on forecast.
-# For hot days we want cooler indoor temperature to lower risk of overheating during the day
-# Todo: if time, add button for booster (add boolean that lowers the setpoint 2 degrees)
+
+"""Calculate setpoint temperature based on forecast.
+    For hot days we want a cooler indoor temperature to 
+    lower the risk of overheating during the day.
+    This settings can be adjusted after some measurings"""
+
+
 def calculateSetPoint(forecast):
     global setPointTemp
-    if forecast>27:
+    """Adjust if needed
+        in my case it usually gets a couple of degrees hotter
+        than the forecast predicts so I adjust for that adding 2 degrees:"""
+    adjustedForecast = forecast+2
+
+    if adjustedForecast>27:
+        setPointTemp = 20
+    elif adjustedForecast >25:
         setPointTemp = 21
-    elif forecast >23:
-        setPointTemp = 22
     else:
         setPointTemp = 23
 
 #Checks if the forecast temperature is high. Returns a boolean (True if hot)
 def checkIsHotDay(forecast):
     isHotDay = False
-    if forecast>25:
+    if forecast>23:
         isHotDay = True
     return isHotDay
 
 #Checks if the conditions are met for opening windows on a hot day forecast
 def hotDaySetting(deltaInSet, deltaInOut):
-    openWindow = False
     '''On a hot day forecast, check these two conditions and return a boolean (openWindow = True) if both are met:
         * temperature indoors - setPoint temperature (desired temperature) is larger than zero
         * temperature indoors - temperature outdoors is larger than zero '''
-    if deltaInSet>0 and deltaInOut>0:
-        openWindow = True
-    return openWindow
+    return deltaInSet>0 and deltaInOut>0
 
 #Checks if the conditions are met for opening the windows on a cool day forecast
 #The reason being, if the indoor temperature is much higher, we may want to cool it down a bit
 def coolDaySetting(deltaInSet, deltaInOut):
-    openWindow = False
     '''On a cool day forecast, check these two conditions and return a boolean (openWindow = True) if both are met:
         * temperature indoors - setPoint temperature (desired temperature) is larger than 2 (so it has to be 3 or more)
         * temperature indoors - temperature outdoors is larger than zero '''    
-    if deltaInSet>2 and deltaInOut>0:
-        openWindow = True
-    return openWindow
+    return deltaInSet>2 and deltaInOut>0
 
 #Check if nightFlush is necessary (if windows should be opened) returns a boolean
 def checkNightFlush(forecast, tempIn, tempOut):
     calculateSetPoint(forecast)
 
-    #Print out data:
-    print("""---\nForecast temperature:{0} C//Indoor: {1} C//Desired: {2}C//Outdoor: {3} C")\n---""".format(forecast, tempIn, setPointTemp, tempOut))
+    #Print out data for testing:
+    #print("""---\nForecast temperature:{0} C//Indoor: {1} C//Desired: {2}C//Outdoor: {3} C")\n---""".format(forecast, tempIn, setPointTemp, tempOut))
     
-    #Delta T in -set = temperature indoors - desired temperature
+    #Delta T in - set = temperature indoors - desired temperature
     deltaTinSet = tempIn - setPointTemp
     
     #Delta T in-out temperature indoors - temperature outdoors
@@ -69,25 +72,15 @@ def actionMessage(windowIsOpen, forecast, tempIn, tempOut):
 
     if not windowIsOpen and nightFlush:
         #code for action - open the window
-        msg = """Conditions for night-flush are good:
-        tomorrow's max-temp is going to be = {0} C,
-        indoor temperature = {1} C (desired temperature = {2} C)
-        outdoor temperature = {3}
-        Open windows for night-flushing""".format(forecast, tempIn, setPointTemp, tempOut )        
-    
+        #msg= f"Open Windows for night flush: Forecast = {forecast} C, T-in = {tempIn} C, Set = {setPointTemp} C, T-out = {tempOut} C"
+        msg = "Open the windows"
     elif windowIsOpen and nightFlush:
-        msg = "Night-flush ON (window is open)"
-
+        msg = "Airing ON (window is open)"
     elif windowIsOpen and not nightFlush:
         #code for action - close the window
-        msg = """Conditions for night-flush are not optimal:
-        tomorrow's max-temp is going to be = {0} C,
-        indoor temperature = {1} C (desired temperature = {2} C)
-        outdoor temperature = {3} 
-        Close windows to avoid overtemperatures""".format(forecast, tempIn, setPointTemp, tempOut )     
-
+        #msg = f"Close Windows to stop night flush: Forecast = {forecast} C, T-in = {tempIn} C, Set = {setPointTemp} C, T-out = {tempOut} C"
+        msg = "Close the windows"    
     else:
-        msg = "Night-flush OFF (window is closed)"    
-    
+        msg = "Airing OFF (window is closed)"    
     return msg
 
